edca36ed4546cf18135266c1c2ff4e9530d87ce0
foorkan <furqanshabbir2010@gmail.com>
2020-01-14 23:42:14 -0600

    first hwk

---
+++ exercise1.py
@@ -0,0 +1,64 @@
+# user_name = input("Enter your name ")
+# print(user_name)
+# print("Hello, " + user_name )
+#number2
+# lengthofname=str(len(user_name))
+# numberofletters = str(lengthofname)
+# message = "your name has " + lengthofname + " in it "
+# print(message.upper())
+#number3
+# The_name = input("what is your name? ")
+# subject = input("what is your facorite subject? ")
+# mySentence = "my name is {} and my favorite subject in school is {}".format(The_name,subject)   
+# print(mySentence)
+#number 4
+# day=int(input('Day (0-6)? '))
+# if (day==0):
+#     print("sunday")
+# if (day==1):
+#     print("monday")    
+# if (day==2):
+#     print("tuesday")
+# if (day==3):
+#     print("wednesday")
+# if (day==4):
+#     print("thursday")
+# if (day==5):
+#     print("friday")
+# if (day==6):
+#     print("saturday")
+# if (day>6):
+#     print("invalid entry")
+# number 5 probably going to be using if elif and else statements (ASK ABOUT THIS AND NUMBER 4)
+# day=int(input("Day (0-6) ? "))
+# if (day==0):
+#     print("sleep in")
+# if (day==1):
+#     print("go to work")    
+# if (day==2):
+#     print("go to work")
+# if (day==3):
+#     print("go to work")
+# if (day==4):
+#     print("go to work")
+# if (day==5):
+#     print("go to work")
+# if (day==6):
+#     print("sleep in")
+# if (day>6):
+#     print("invalid entry")
+#number 6
+# Temperature=int(input("what is the temperature in celsius? "))
+# Temperature=(Temperature*1.8)+32
+# print(Temperature)
+# count = 0
+# while(count<=10):
+#     print("the count is " + str(count))
+#     count=count+1
+#medium
+bill_amount=int(input("total bill? "))
+level_of_service=input("what was the level of the service? ")
+float good = .20
+float fair = .15
+float bad = .10
+casted_output=
